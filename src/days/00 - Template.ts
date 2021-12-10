232e0e9c9249002f4e454b2e289279fc8b3bc67e
TheIcyStar <theicystar@users.noreply.github.com>
2021-12-10 15:12:46 -0500

    add files

---
+++ src/days/00 - Template.ts
@@ -0,0 +1,9 @@
+/* Day 00 - Day template
+    This is the template for a day's module
+    Each module has a Run() function that gets called with data from the index script
+*/
+
+
+export function Run(data: string){
+    console.log("Hello world from day 00!")
+}
