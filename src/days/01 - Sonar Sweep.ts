b5c192194932ab26c18a7bf59fb52e50b7289bf6
TheIcyStar <theicystar@users.noreply.github.com>
2021-12-11 14:51:43 -0500

    part 1 solution

---
+++ src/days/01 - Sonar Sweep.ts
@@ -0,0 +1,71 @@
+/* Day 01 - Sonar Sweep
+    As the submarine drops below the surface of the ocean, it automatically performs a sonar sweep of the nearby sea floor. On a small screen, the sonar sweep report (your puzzle input) appears: each line is a measurement of the sea floor depth as the sweep looks further and further away from the submarine.
+
+For example, suppose you had the following report:
+
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263
+This report indicates that, scanning outward from the submarine, the sonar sweep found depths of 199, 200, 208, 210, and so on.
+
+The first order of business is to figure out how quickly the depth increases, just so you know what you're dealing with - you never know if the keys will get carried into deeper water by an ocean current or a fish or something.
+
+To do this, count the number of times a depth measurement increases from the previous measurement. (There is no measurement before the first measurement.) In the example above, the changes are as follows:
+
+199 (N/A - no previous measurement)
+200 (increased)
+208 (increased)
+210 (increased)
+200 (decreased)
+207 (increased)
+240 (increased)
+269 (increased)
+260 (decreased)
+263 (increased)
+In this example, there are 7 measurements that are larger than the previous measurement.
+
+How many measurements are larger than the previous measurement?
+*/
+import { assert } from "console"
+
+
+export function Run(data: string){
+    let dataArray = data.split(/\r?\n/)
+    let increases = 0
+    let decreases = 0
+    let prev = null
+
+    //Part two Variables
+    let window = []
+
+    for(const measure of dataArray){
+        let curNum = parseInt(measure)
+        assert(curNum, "Could not convert measure to number: "+measure)
+
+        //first measure
+        if(!prev){
+            prev = curNum
+            continue
+        }
+        //comparison
+        if(curNum > prev){
+            increases++
+        } else {
+            decreases++
+        }
+
+        prev = curNum
+    }
+
+    console.log("===== RESULT =====")
+    console.log("Total data points: "+dataArray.length)
+    console.log("Increases: "+increases)
+    console.log("Decreases: "+decreases)
+}
