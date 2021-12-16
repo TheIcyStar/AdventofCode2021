febed73da93e033a1468d0458860d7414fdb7483
TheIcyStar <theicystar@users.noreply.github.com>
2021-12-15 19:52:49 -0500

    day 2 part 2 & small cleanup

--- src/days/02 - Dive!.ts
+++ src/days/02 - Dive!.ts
@@ -8,16 +8,44 @@ export function RunA(data: string){
 
     for(let i=0; i<dataArray.length; i++){
         let split = dataArray[i].split(" ")
+        let delta = parseInt(split[1])
+
         if(split[0] === "forward"){
-            distance += parseInt(split[1])
+            distance += delta
         } else if(split[0] === "down") {
-            depth += parseInt(split[1])
+            depth += delta
         } else if(split[0] === "up") {
-            depth -= parseInt(split[1])
+            depth -= delta
         }
     }
 
     console.log("===== PART 1 RESULT =====")
+    console.log("distance: "+distance)
+    console.log("depth: "+depth)
+    console.log("Multiply result: "+distance * depth)
+}
+
+export function RunB(data: string){
+    let dataArray = data.split(/\r?\n/)
+    let distance = 0
+    let depth = 0
+    let aim = 0
+
+    for(let i=0; i<dataArray.length; i++){
+        let split = dataArray[i].split(" ")
+        let delta = parseInt(split[1])
+
+        if(split[0] === "forward"){
+            distance += delta
+            depth += (aim * delta)
+        } else if(split[0] === "down") {
+            aim += delta
+        } else if(split[0] === "up") {
+            aim -= delta
+        }
+    }
+
+    console.log("===== PART 2 RESULT =====")
     console.log("distance: "+distance)
     console.log("depth: "+depth)
     console.log("Multiply result: "+distance * depth)
