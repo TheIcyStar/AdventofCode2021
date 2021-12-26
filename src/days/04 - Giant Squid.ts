06692b32e764613f984e09ff2b80ba018b45d848
TheIcyStar <theicystar@users.noreply.github.com>
2021-12-25 21:18:39 -0500

    Day 2 part 2 and Bingo detection improvement

--- src/days/04 - Giant Squid.ts
+++ src/days/04 - Giant Squid.ts
@@ -16,16 +16,43 @@ class BingoBoard {
         }
     }
 
-    handleCall(call: string): boolean{
+    handleCall(call: string){
         for(var x=0; x<this.grid.length; x++){
             for(var y=0; y<this.grid[x].length; y++){
                 //If we Found the number, no need to check anything else. Return.
                 if(this.grid[x][y] === call){
-                    this.grid[x].splice(y,1)
-                    return (this.grid[x].length === 0)
+                    this.grid[x][y] = "x"
+                    return
                 }
             }
         }
+    }
+
+    isBingo(): boolean{
+        //rowcheck
+        for(var x=0; x<this.grid.length; x++){
+            let rowcheckBreak = false
+            for(var y=0; y<this.grid[x].length; y++){
+                if(this.grid[x][y] !== "x"){
+                    rowcheckBreak = true
+                    break
+                }
+            }
+            if(!rowcheckBreak) return true
+        }
+
+        //columncheck  //we do a little swapping
+        for(var x=0; x<this.grid.length; x++){
+            let columncheckBreak = false
+            for(var y=0; y<this.grid[x].length; y++){
+                if(this.grid[y][x] !== "x"){
+                    columncheckBreak = true
+                    break
+                }
+            }
+            if(!columncheckBreak) return true
+        }
+
         return false
     }
 
@@ -33,6 +60,8 @@ class BingoBoard {
         let sum = 0
         for(const row of this.grid){
             for(const numStr of row){
+                if(numStr === "x") continue
+                
                 sum += parseInt(numStr)
             }
         }
@@ -64,8 +93,8 @@ export function RunA(data: string){
         for(const card of bingoCards){
             if(winningCard) continue
 
-            let isBingo = card.handleCall(call)
-            if(isBingo){
+            card.handleCall(call)
+            if(card.isBingo()){
                 winningCard = card
                 break
             }
@@ -77,6 +106,39 @@ export function RunA(data: string){
             console.log("Winning call: "+call)
             console.log("Final score: "+winningCard.getScore(parseInt(call)))
             return
+        }
+    }
+}
+
+export function RunB(data: string){
+    let dataArray = data.split(/\r?\n/)
+    let bingoCalls = dataArray[0].split(",")
+
+    //create bingo cards
+    for(let i=2; i < dataArray.length; i+= 6){
+        let boardNumbers: string[] = Array.prototype.concat(dataArray[i],dataArray[i+1],dataArray[i+2],dataArray[i+3],dataArray[i+4])
+        let newBingoCard = new BingoBoard(boardNumbers)
+        bingoCards.push(newBingoCard)
+    }
+
+    //Iterate through each card and remove them one by one
+    for(const call of bingoCalls){
+        for(let cardIndex=0; cardIndex<bingoCards.length;){
+
+            bingoCards[cardIndex].handleCall(call)
+            if(bingoCards[cardIndex].isBingo()){
+                //calculate result
+                if(bingoCards.length > 1){
+                    bingoCards.splice(cardIndex,1)
+                    continue
+                } else {
+                    console.log("===== PART 2 RESULTS =====")
+                    console.log("Final winning call: "+call)
+                    console.log("Final last winner score: "+bingoCards[0].getScore(parseInt(call)))
+                    return
+                }
+            }
+            cardIndex++
         }
     }
 }
